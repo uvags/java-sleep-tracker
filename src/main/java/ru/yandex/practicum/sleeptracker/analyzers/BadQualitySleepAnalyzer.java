294b31708550236eb4c5dab0e5918bbe7a25175d
uvags <uvadev@yandex.ru>
2026-09-07 04:16:28 +0700

    style: added empty line between import and class declaration

--- src/main/java/ru/yandex/practicum/sleeptracker/analyzers/BadQualitySleepAnalyzer.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/analyzers/BadQualitySleepAnalyzer.java
@@ -1,8 +1,9 @@
 package ru.yandex.practicum.sleeptracker.analyzers;
+
 import ru.yandex.practicum.sleeptracker.SleepQuality;
 import ru.yandex.practicum.sleeptracker.SleepingSession;
-
 import java.util.List;
+
 public class BadQualitySleepAnalyzer implements SleepAnalyzer {
     @Override
     public SleepAnalysisResult<Long> apply(List<SleepingSession> sessions) {
